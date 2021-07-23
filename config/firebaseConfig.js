3135d87cd784d99b4a458227894545a870f831cb
chloesyy <chlyiyi@hotmail.com>
2021-07-23 16:56:42 +0800

    add configfile

---
+++ config/firebaseConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCdZVc8PqRTAuYAJgXrJdTJHFnY6EOM7sk",
+    authDomain: "jioleh-4988a.firebaseapp.com",
+    projectId: "jioleh-4988a",
+    storageBucket: "jioleh-4988a.appspot.com",
+    messagingSenderId: "315261928107",
+    appId: "1:315261928107:web:96842fe83b27919e9cedee",
+    measurementId: "G-1RZDXYLWJX"
+  };
+
