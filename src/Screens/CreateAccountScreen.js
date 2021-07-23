d42954fb5644a8ff1740392aab8cd50a0162525c
chloesyy <chlyiyi@hotmail.com>
2021-07-23 18:48:52 +0800

    add createaccount backend

--- src/Screens/CreateAccountScreen.js
+++ src/Screens/CreateAccountScreen.js
@@ -24,114 +24,114 @@ const config = {
 };
 
 export default class CreateAccountScreen extends Component {
-  isUserEqual = (googleUser, firebaseUser) => {
-  if (firebaseUser) {
-    var providerData = firebaseUser.providerData;
-    for (var i = 0; i < providerData.length; i++) {
-      if (providerData[i].providerId === 
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-        providerData[i].uid === 
-        googleUser.uid
-      ) {
-        // We don't need to reauth the Firebase connection.
-        return true;
-      }
-    }
-  }
-  return false;
-};
+    isUserEqual = (googleUser, firebaseUser) => {
+        if (firebaseUser) {
+            var providerData = firebaseUser.providerData;
+            for (var i = 0; i < providerData.length; i++) {
+            if (providerData[i].providerId === 
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                providerData[i].uid === 
+                googleUser.uid
+            ) {
+                // We don't need to reauth the Firebase connection.
+                return true;
+            }
+            }
+        }
+        return false;
+    };  
 
-onSignIn = (googleUser) => {
-  console.log("Google Auth Response");
-  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-  var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
-    unsubscribe();
-    // Check if we are already signed-in Firebase with the correct user.
-    if (!this.isUserEqual(googleUser, firebaseUser)) {
-      // Build Firebase credential with the Google ID token.
-      var credential = firebase.auth.GoogleAuthProvider.credential(
-        googleUser.idToken,
-        googleUser.accessToken, 
-      );
-      // Sign in with credential from the Google user.
-      firebase.auth().signInWithCredential(credential)
-        .then((result) => {
-          console.log("user signed in");
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    } else {
-      console.log("User already has an account Firebase.");
-    }
-  })
-  .bind(this);
-};
+    onSignIn = (googleUser) => {
+    console.log("Google Auth Response");
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+        unsubscribe();
+        // Check if we are already signed-in Firebase with the correct user.
+        if (!this.isUserEqual(googleUser, firebaseUser)) {
+        // Build Firebase credential with the Google ID token.
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+            googleUser.idToken,
+            googleUser.accessToken, 
+        );
+        // Sign in with credential from the Google user.
+        firebase.auth().signInWithCredential(credential)
+            .then((result) => {
+            console.log("user signed in");
+            })
+            .catch(function (error) {
+            console.log(error);
+            });
+        } else {
+        console.log("User already has an account Firebase.");
+        }
+    })
+    .bind(this);
+    };
 
-signInWithGoogleAsync = async () => {
-  try {
-    const result = await Google.logInAsync(config);
-    if (result.type === "success") {
-      this.onSignIn(result);
-      return result.accessToken;
-    } else {
-      console.log("log in failed: " + result.type)
-      return { cancelled: true };
+    signInWithGoogleAsync = async () => {
+    try {
+        const result = await Google.logInAsync(config);
+        if (result.type === "success") {
+        this.onSignIn(result);
+        return result.accessToken;
+        } else {
+        console.log("log in failed: " + result.type)
+        return { cancelled: true };
+        }
+    } catch (e) {
+        console.log(e)
+        return { error: true };
     }
-  } catch (e) {
-    console.log(e)
-    return { error: true };
-  }
-};
+   };
 
-render() {
-    return (
-        <DismissKeyboardView style={styles.container}>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <FontAwesome5 name="chevron-left" size={25} style={styles.chevron}/>
-            </TouchableOpacity>
-            <View style={styles.header}>
-            <Text style={styles.title}>Registration</Text>
-            <View style={styles.form}>
-              <Text style={styles.name}>Email Address</Text>
-              <TextInput
-                style={styles.input}
-                placeholder=" Enter Email"
-              >
-              </TextInput>
-              <Text style={styles.name}>Password</Text>
-              <TextInput
-                style={styles.input}
-                placeholder=" Enter Password"
+    render() {
+        return (
+            <DismissKeyboardView style={styles.container}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+                <FontAwesome5 name="chevron-left" size={25} style={styles.chevron}/>
+                </TouchableOpacity>
+                <View style={styles.header}>
+                <Text style={styles.title}>Registration</Text>
+                <View style={styles.form}>
+                <Text style={styles.name}>Email Address</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder=" Enter Email"
                 >
-              </TextInput>
-              <Text style={styles.name}>Confirm Password</Text>
-              <TextInput
-                style={styles.input}
-                placeholder=" Re-enter Password"
-                >
-              </TextInput>
-              </View>
-              <TouchableOpacity style={styles.buttonInverted} onPress={() => navigation.navigate('SelectGame')}>
-                  <Text style={styles.invertedText}>Continue</Text>
-              </TouchableOpacity>
-                <View style={styles.googleBlock}>
-                    <Image 
-                        style={styles.img}
-                        source={require("../../assets/google.png")}></Image>
-                    <TouchableOpacity
-                        onPress={() => {
-                            console.log("pressed")
-                            this.signInWithGoogleAsync()
-                        }}
+                </TextInput>
+                <Text style={styles.name}>Password</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder=" Enter Password"
                     >
-                        <Text style={styles.googleText}>Continue with Google</Text>
-                    </TouchableOpacity>
+                </TextInput>
+                <Text style={styles.name}>Confirm Password</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder=" Re-enter Password"
+                    >
+                </TextInput>
                 </View>
-            </View>
-        </DismissKeyboardView>
-      )
-  }
+                <TouchableOpacity style={styles.buttonInverted} onPress={() => navigation.navigate('SelectGame')}>
+                    <Text style={styles.invertedText}>Continue</Text>
+                </TouchableOpacity>
+                    <View style={styles.googleBlock}>
+                        <Image 
+                            style={styles.img}
+                            source={require("../../assets/google.png")}></Image>
+                        <TouchableOpacity
+                            onPress={() => {
+                                console.log("pressed")
+                                this.signInWithGoogleAsync()
+                            }}
+                        >
+                            <Text style={styles.googleText}>Continue with Google</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </DismissKeyboardView>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
