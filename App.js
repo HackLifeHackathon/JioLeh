47aaa7061c8e42887d9a2e1d3900e4aea15f3d2e
Tasha Marion Wan <tas.wan23@gmail.com>
2021-07-23 14:07:59 +0800

    Create account screen

--- App.js
+++ App.js
@@ -2,10 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './src/Screens/LoginScreen';
+import CreateAccountScreen from './src/Screens/CreateAccountScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <LoginScreen/>
+    <NavigationContainer>
+      <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="FirstScreen" component={LoginScreen} />
+        <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/Screens/CreateAccountScreen.js
+++ src/Screens/CreateAccountScreen.js
@@ -1,20 +1,81 @@
 import React from 'react';
 import { Ionicons, FontAwesome5 } from '@expo/vector-icons'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Formik } from 'formik';
 
-export default function CreateAccountScreen() {
+export default function CreateAccountScreen({ navigation }) {
     return (
         <View style={styles.container}>
-            <FontAwesome5 name="chevron-left" />
-            <Text style={styles.header}>Create Account</Text>
-            <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>CREATE ACCOUNT</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.buttonInverted}>
-                <Text style={styles.invertedText}>SIGN IN</Text>
-            </TouchableOpacity>
-            </View>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <FontAwesome5 name="chevron-left" size={25} style={styles.chevron}/>
+            <Formik
+        initialValues={{
+          description: '',
+          curEmail: profile?.emails[0],
+        }}
+        validationSchema={FormSchema}
+        validateOnChange
+        onSubmit={() => {
+          onSubmit()
+        }}
+      >
+        {(formikProps) => (
+          <>
+            <KeyboardAvoidingView
+              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+              style={styles.keyboardAvoidingViewContainer}
+            >
+              <ScrollView style={styles.content}>
+
+                <View style={styles.contentNewPassword}>
+                  <View style={styles.formContainer}>
+                    <FormField
+                      title="Email Address"
+                      email
+                      enabled={false}
+                      defaultValue={formikProps.values.curEmail}
+                      border={0}
+                    />
+                    <FormField
+                      title={translate('description')}
+                      validateDelay={false}
+                      onChangeText={(text) => {
+                        formikProps.handleChange('description')(text)
+                        setDescription(text)
+                      }}
+                      isTouched={() => formikProps.setFieldTouched('description', true)}
+                      placeholder={translate('description')}
+                      touched={formikProps.touched.description}
+                      error={description === '' && formikProps.errors.description}
+                      showApiErrors
+                      multiline
+                    />
+                    <FormFieldImagesUpload onChangeImages={(images) => {
+                      setImages(images)
+                    }}
+                    />
+                  </View>
+                </View>
+                {showAction && (
+                <View style={styles.relativeFooterContainer}>
+                  <Button
+                    onPress={() => {
+                      formikProps.handleSubmit()
+                    }}
+                    style={ownStyles.footButton}
+                    loading={loading}
+                    disabled={!isAllowToSubmit}
+                  >
+                    {translate('submit')}
+                  </Button>
+                </View>
+                )}
+              </ScrollView>
+            </KeyboardAvoidingView>
+          </>
+        )}
+      </Formik>
+          </TouchableOpacity>
         </View>
     )
 }
@@ -22,7 +83,11 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#a8c961',
-      alignItems: 'center',
+      //alignItems: 'center',
       //justifyContent: 'center',
+    },
+    chevron: {
+        top: 70,
+        paddingLeft: 30,
     },
   });
